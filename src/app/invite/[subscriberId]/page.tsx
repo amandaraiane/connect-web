7a29eda577e32e9575db2b46526d0a8852d08ed7
amanda-raiane <amanda@codifica.dev>
2025-02-22 23:12:47 -0300

    chore: create dynamic route

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
-import { Ranking } from "./ranking";
-import logo from "../../assets/logo.svg";
+
+import logo from "../../../assets/logo.svg";
 import { Stats } from "./stats";
 import { InviteLinkInput } from "./invite-link-input";
+import { Ranking } from "./ranking";
 
-export default function InvitePage() {
-  const inviteLink = "http://localhost:3000/invite/87389573489";
+interface InvitePageProps {
+  params: Promise<{
+    subscriberId: string;
+  }>;
+}
+export default async function InvitePage(props: InvitePageProps) {
+  const { subscriberId } = await props.params;
+
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`;
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
