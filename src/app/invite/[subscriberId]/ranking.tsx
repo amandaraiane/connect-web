2a37382da47ca45e0770ca971786f825c8448c44
amanda-raiane <amanda@codifica.dev>
2025-02-23 09:16:08 -0300

    chore: search ranking data and statistics

--- src/app/invite/[subscriberId]/ranking.tsx
+++ src/app/invite/[subscriberId]/ranking.tsx
@@ -1,48 +1,46 @@
 import Image from "next/image";
+import { getRanking } from "@/http/api";
+
 import gold from "../../../assets/medal-gold.svg";
 import silver from "../../../assets/medal-silver.svg";
 import cooper from "../../../assets/medal-cooper.svg";
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking();
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
         Ranking de indicações
       </h2>
       <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-8 flex flex-col justify-center gap-2">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1°</span> | Amanda Raiane
-          </span>
+        {ranking.map((item, index) => {
+          const rankingPosition = index + 1;
+          return (
+            <div
+              key={item.id}
+              className="relative rounded-xl bg-gray-700 border border-gray-600 p-8 flex flex-col justify-center gap-2"
+            >
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}°</span> |
+                {item.name}
+              </span>
 
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1090
-          </span>
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                {item.score}
+              </span>
 
-          <Image src={gold} alt="" className="absolute top-0 right-8" />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-8 flex flex-col justify-center gap-2">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2°</span> | Amanda Raiane
-          </span>
-
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1090
-          </span>
-
-          <Image src={silver} alt="" className="absolute top-0 right-8" />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-8 flex flex-col justify-center gap-2">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3°</span> | Amanda Raiane
-          </span>
-
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1090
-          </span>
-
-          <Image src={cooper} alt="" className="absolute top-0 right-8" />
-        </div>
+              {rankingPosition === 1 && (
+                <Image src={gold} alt="" className="absolute top-0 right-8" />
+              )}
+              {rankingPosition === 2 && (
+                <Image src={silver} alt="" className="absolute top-0 right-8" />
+              )}
+              {rankingPosition === 3 && (
+                <Image src={cooper} alt="" className="absolute top-0 right-8" />
+              )}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
